b602206c42f998d65d758b39965d2501bf2ea8f1
Noob\x20Code <41012866+n0obit4@users.noreply.github.com>
2020-09-14 18:38:55 +0000

    Update fuzzpy.py

--- fuzzpy.py
+++ fuzzpy.py
@@ -11,6 +11,18 @@ import requests,sys
 from time import sleep
 from colorama import Fore
 
+def banner():
+	print(r'''	
+    ______                           
+   / ____/_  __________  ____  __  __
+  / /_  / / / /_  /_  / / __ \/ / / /
+ / __/ / /_/ / / /_/ /_/ /_/ / /_/ / 
+/_/    \__,_/ /___/___/ .___/\__, /  
+                     /_/    /____/   
+		Created By: n0obit4
+''')
+	print("Github: https://github.com/n0obit4")
+	
 def fuzz(url,file):
 	header = {'User-agent': 'Mozilla/5.0'}
 
